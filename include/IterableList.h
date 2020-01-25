1fd168243d6f4b7e350cd6bb44e38c415daa6bd5
Sunil Singh <sunilp896@gmail.com>
2020-01-25 11:20:35 +0530

    changes the declaration of IterableList. Now it is a abstract class, with no data storage. For now only InputIterator operators are included.

--- include/IterableList.h
+++ include/IterableList.h
@@ -9,8 +9,8 @@
  * 
  */
 
-#ifndef _A_ITERABLE_LIST_H_
-#define _A_ITERABLE_LIST_H_
+#ifndef _A_ITERABLE_H_
+#define _A_ITERABLE_H_
 
 #include <iostream>
 #include "llvm/IR/Value.h"
@@ -21,111 +21,17 @@ using namespace std;
 namespace A{
 
 template<typename T>
-class IterableList
+class Iterable
 {
-    class Node;
-
 public:
-    IterableList() noexcept
-    {
-        pRootNode = nullptr;
-    }
-
-    class Iterator;
-
-
-    Iterator begin()
-    {
-        return Iterator(pRootNode);
-    }
-
-    Iterator end()
-    {
-        return Iterator(nullptr);
-    }
-
-    void push_back(T data);
-
-    class Iterator
-    {
-    public:
-        Iterator() noexcept:
-	    currentNode(pRootNode){ }
-
-        Iterator(const Node* pNode) noexcept: 
-	    currentNode(pNode){	}
-
-        Iterator& operator=(Node* node)
-        {
-            this->currentNode = node;
-            return *this;
-        }
-
-        // prefix
-        Iterator& operator++()
-        {
-            if(currentNode)
-                currentNode = currentNode->next;
-            return *this;
-        }
-
-        // postfix : must be non reference
-        Iterator operator++(int)
-        {
-            Iterator it = *this;
-            ++(*this);
-            return it;
-        }
-
-        bool operator!=(const Iterator& it)
-        {
-            return this->currentNode != it.currentNode;
-        }
-
-	bool operator==(const Iterator& it)
-	{
-	    return this->currentNode == it.currentNode;
-	}
-
-        T operator*()
-        {
-            return currentNode->data;
-        }
-
-
-    private:
-        const Node* currentNode;
-    };
-    
+    virtual Iterable& operator=(T n) = 0;
+    virtual Iterable& operator++() = 0;
+    virtual Iterable& operator++(int) = 0;
+    virtual bool operator!=(const Iterable& it) = 0;
+    virtual bool operator==(const Iterable& it) = 0;
+    virtual T operator*() = 0;
 private:
-    class Node
-    {
-        T data;
-        Node* next;
-
-        friend class IterableList;
-    };
-
-    Node* getNode(T d)
-    {
-        Node *newNode = new Node;
-        newNode->data = d;
-        newNode->next = nullptr;
-
-        return newNode;
-    }
-
-    Node*& getRootNode()
-    {
-        return pRootNode;
-    }
-    
-    static Node* pRootNode;
-
 };
-
-template <typename T>
-typename IterableList<T>::Node* IterableList<T>::pRootNode = nullptr;
 
 }   //  
 
