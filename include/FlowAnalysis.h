9cdbad8d6e3c5f23d7e8b5e3d4ebccac4ab3b1fd
Sunil Singh <sunilp896@gmail.com>
2020-02-25 08:09:14 +0530

    1. Removed the overriden function, need to be implemented in the outer most implementation by the user. 2. added functions NodesDataList: create a NodeData for each BB, visits in DF traversal. AddOutData: visits each out BB of a nodedata and creates a Nodedata object for that BB CreateAndReturnNodeData: create NodeData for each BB.

--- include/FlowAnalysis.h
+++ include/FlowAnalysis.h
@@ -12,6 +12,10 @@
 #define _A_FLOW_ANALYSIS_H_
 
 #include "AbstractFlowAnalysis.h"
+#include "NodeData.h"
+#include <list>
+#include <stack>
+#include "llvm/ADT/SmallVector.h"
 
 namespace A{
     /**
@@ -25,15 +29,21 @@ namespace A{
         FlowAnalysis(){}
         ~FlowAnalysis(){}
 
-        T NewInitialFlowSet() override;
-        T EntryInitialFlowSet() override;
-        void Merge(T in1, T in2, T out) override;
-        void Copy(T in1, T in2) override;
+        // T NewInitialFlowSet() override;
+        // T EntryInitialFlowSet() override;
+        // void Merge(T in1, T in2, T out) override;
+        // void Copy(T in1, T in2) override;
         T GetFlowBefore(llvm::BasicBlock* node) override;
 
         void Compute();
 
     private:
+        std::vector<A::NodeData<T>*> NodesDataList();
+        void AddOutData(llvm::DenseMap<llvm::BasicBlock*,A::NodeData<T>*>&, 
+                                                        std::vector<llvm::BasicBlock*>&, A::NodeData<T>*);
+        A::NodeData<T>* CreateAndReturnNodeData(llvm::DenseMap<llvm::BasicBlock*,A::NodeData<T>*>&, 
+                                                        llvm::BasicBlock*, A::NodeData<T>*);
+
         llvm::DenseMap<llvm::BasicBlock*,T> finalInSet;
         llvm::DenseMap<llvm::BasicBlock*,T> finalOutSet;
     };
--- src/FlowAnalysis.cpp
+++ src/FlowAnalysis.cpp
@@ -7,53 +7,6 @@
  * @brief 
  * 
  * @tparam T 
- * @return T 
- */
-template<typename T>
-T A::FlowAnalysis<T>::NewInitialFlowSet(){
-
-}
-
-/**
- * @brief 
- * 
- * @tparam T 
- * @return T 
- */
-template<typename T>
-T A::FlowAnalysis<T>::EntryInitialFlowSet(){
-
-}
-
-/**
- * @brief 
- * 
- * @tparam T 
- * @param in1 
- * @param in2 
- * @param out 
- */
-template<typename T>
-void A::FlowAnalysis<T>::Merge(T in1, T in2, T out){
-
-}
-
-/**
- * @brief 
- * 
- * @tparam T 
- * @param in1 
- * @param in2 
- */
-template<typename T>
-void A::FlowAnalysis<T>::Copy(T in1, T in2){
-
-}
-
-/**
- * @brief 
- * 
- * @tparam T 
  * @param node 
  * @return T 
  */
@@ -69,12 +22,85 @@ T A::FlowAnalysis<T>::GetFlowBefore(llvm::BasicBlock* node){
  */
 template<typename T>
 void A::FlowAnalysis<T>::Compute(){
-    llvm::Function* func = this->func;
-    llvm::errs() << func->getName().str()<<'\n';
+    // llvm::Function* func = this->func;
+    std::vector<A::NodeData<T>*> lstNodes = NodesDataList();
+}
+
+template<typename T>
+std::vector<A::NodeData<T>*> A::FlowAnalysis<T>::NodesDataList(){
+    std::vector<A::NodeData<T>*> l;
+    llvm::errs() << this->func->size() <<'\n';
+    
+    std::vector<llvm::BasicBlock*> entries;
+    A::FunctionCFG* g = this->graph;
+    if(this->IsForward()){
+        entries = g->GetHeads();
+    }else{
+        entries = g->GetTails();
+    }
+
+    llvm::DenseMap<llvm::BasicBlock*,A::NodeData<T>*> visited;
+    int n = -1;
+    A::NodeData<T> *dummyEntry = new A::NodeData<T>();
+
+    AddOutData(visited,entries,dummyEntry);
+    dummyEntry->MarkVisit(++n);
+    dummyEntry->SetInFlow(this->EntryInitialFlowSet());
+    dummyEntry->SetOutFlow(this->EntryInitialFlowSet());
+
+    std::stack<A::NodeData<T>*> visitStack;
+    unsigned i = 0;
+    A::NodeData<T> *v = dummyEntry;
+    visitStack.push(dummyEntry);
+
+    while(!visitStack.empty()){
+        A::NodeData<T>* s = visitStack.top();
+        visitStack.pop();
+
+        if(!s->IsVisited()){
+            s->MarkVisit(++n);
+            std::vector<llvm::BasicBlock*> outs = g->GetOut(g->GetNode(s->GetNode()),A::Direction::BACKWARD);
+            AddOutData(visited,outs,s);
+        }
+
+        llvm::SmallVector<A::NodeData<T>*,0> tmp = s->GetOutgoingN();
+        for(auto i=0; i<tmp.size(); ++i){
+            A::NodeData<T>* w  = tmp[i];
+            if(!w->IsVisited())
+            visitStack.push(w);
+        }
+        l.push_back(s);
+    }
+
+    return l;
+}
+
+template<typename T>
+void A::FlowAnalysis<T>::AddOutData(llvm::DenseMap<llvm::BasicBlock*,A::NodeData<T>*>& visited, 
+                                                std::vector<llvm::BasicBlock*>& blocks, A::NodeData<T>* nd){
+    llvm::SmallVector<A::NodeData<T>*,0> out;
+    
+    for(unsigned i=0; i<blocks.size(); ++i){
+        out.push_back(CreateAndReturnNodeData(visited,blocks[i],nd));
+    }
+    nd->AddOut(out);
 }
 
 
+template<typename T>
+A::NodeData<T>* A::FlowAnalysis<T>::CreateAndReturnNodeData(llvm::DenseMap<llvm::BasicBlock*,A::NodeData<T>*>& visited, 
+                                                        llvm::BasicBlock* entry, A::NodeData<T> *node){
+    if(!visited[entry]){
+        A::NodeData<T> *nd = new A::NodeData<T>(entry,node);
+        visited[entry] = nd;
+        return nd;
+    }else{
+        A::NodeData<T> *oldNodeData = visited[entry];
+        if(oldNodeData != node)
+            oldNodeData->AddIn(node);
+        return oldNodeData;
+    }
+}
+
 template class A::FlowAnalysis<A::ArrayPackedSet<char*>>;
 template class A::FlowAnalysis<A::ArraySparseSet<char*>>;
-template class A::FlowAnalysis<int>;
-template class A::FlowAnalysis<char*>;
