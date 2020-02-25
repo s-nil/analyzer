245b9513e457ddb3f930d048fda70413415c19cd
Sunil Singh <sunilp896@gmail.com>
2020-02-25 08:12:02 +0530

    Adds more accessor functions to nodedata and marking a nodedata visited with number

--- include/NodeData.h
+++ include/NodeData.h
@@ -1,28 +1,64 @@
+/**
+ * @file NodeData.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-02-21
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 #ifndef _A_NODE_DATA_H_
 #define _A_NODE_DATA_H_
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/IR/BasicBlock.h"
 
 namespace A{
+    /**
+     * @brief 
+     * 
+     * @tparam T 
+     */
     template<typename T>
     class NodeData{
     public:
         NodeData(){
             node = nullptr;
         }
-        NodeData(llvm::BasicBlock* n){
+        NodeData(llvm::BasicBlock* n, NodeData<T>* in){
             node = n;
+            if(in) AddIn(in);
+            number = INT_MIN;
         }
-        inline std::string GetNodeName();
-        inline T GetInFlow();
-        inline T GetOutFlow();
+        std::string GetNodeName();
+        T GetInFlow(){   return inFlow;}
+        T GetOutFlow(){  return outFlow;}
+        void SetInFlow(T in){    inFlow = in;}
+        void SetOutFlow(T out){  outFlow = out;}
+        llvm::SmallVector<NodeData<T>*,0> GetINcomingN(){    return incoming;}
+        llvm::SmallVector<NodeData<T>*,0> GetOutgoingN(){    return outgoing;}
+        llvm::BasicBlock* GetNode(){    /*if(node)llvm::errs() << ":: "<<node->getName().str()<<'\n';*/ return node;}
+        llvm::SmallVector<llvm::BasicBlock*,0> GetINcoming();
+        llvm::SmallVector<llvm::BasicBlock*,0> GetOutgoing();
+        void AddIn(NodeData<T>*);
+        void AddIn(llvm::SmallVector<NodeData<T>*,0>);
+        void AddOut(NodeData<T>*);
+        void AddOut(llvm::SmallVector<NodeData<T>*,0>);
+        bool IsVisited(){  return number != INT_MIN;}
+        void MarkVisit(int n){   number=n;}
         ~NodeData(){}
     private:
         llvm::BasicBlock* node;
+        /// final in flow after merging all incoming flow data
         T inFlow;
+        /// out flow after applying transfer function to the inFlow
         T outFlow;
-        llvm::SmallVector<NodeData*,0>  in;
-        llvm::SmallVector<NodeData*,0>  out;
+        /// data of incoming edges
+        llvm::SmallVector<NodeData<T>*,0>  incoming;
+        /// data of outgoing edges
+        llvm::SmallVector<NodeData<T>*,0>  outgoing;
+        ///
+        int number;
     };
     
 }
--- src/NodeData.cpp
+++ src/NodeData.cpp
@@ -2,19 +2,89 @@
 #include "ArrayPackedSet.h"
 #include "ArraySparseSet.h"
 
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ * @return std::string 
+ */
 template<typename T>
-std::string A::NodeData<T>::GetNodeName(){
-    return node->getName().str();
+inline std::string A::NodeData<T>::GetNodeName(){
+    if(node)
+        return node->getName().str();
+    return "";
 }
 
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ */
 template<typename T>
-T A::NodeData<T>::GetInFlow(){
-    return inFlow;
+inline void A::NodeData<T>::AddIn(A::NodeData<T>* n){
+    incoming.push_back(n);
 }
 
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ */
 template<typename T>
-T A::NodeData<T>::GetOutFlow(){
-    return outFlow;
+void A::NodeData<T>::AddIn(llvm::SmallVector<A::NodeData<T>*,0> list){
+    for(auto I=list.begin(),E=list.end(); I!=E; ++I){
+        AddIn(*I);
+    }
+}
+
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ */
+template<typename T>
+inline void A::NodeData<T>::AddOut(A::NodeData<T>* n){
+    outgoing.push_back(n);
+}
+
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ */
+template<typename T>
+void A::NodeData<T>::AddOut(llvm::SmallVector<A::NodeData<T>*,0> list){
+    for(auto I=list.begin(),E=list.end(); I!=E; ++I){
+        AddOut(*I);
+    }
+}
+
+/**
+ * @brief 
+ * 
+ * @return llvm::SmallVector<llvm::BasicBlock*,0> 
+ */
+template<typename T>
+llvm::SmallVector<llvm::BasicBlock*,0> A::NodeData<T>::GetINcoming(){
+    llvm::SmallVector<llvm::BasicBlock*,0> tmp;
+    for(auto I = incoming.begin(), IE = incoming.end(); I!=IE; ++I ){
+        tmp.push_back((*I)->node);
+    }
+    return tmp;
+}
+
+/**
+ * @brief Get the Outgoing object
+ * 
+ * @return llvm::SmallVector<llvm::BasicBlock*,0> 
+ */
+template<typename T>
+llvm::SmallVector<llvm::BasicBlock*,0> A::NodeData<T>::GetOutgoing(){
+llvm::SmallVector<llvm::BasicBlock*,0> tmp;
+    for(auto I = outgoing.begin(), IE = outgoing.end(); I!=IE; ++I ){
+        tmp.push_back((*I)->node);
+    }
+    return tmp;
 }
 
 template class A::NodeData<A::ArrayPackedSet<char*>>;
