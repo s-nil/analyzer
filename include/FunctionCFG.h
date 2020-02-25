69bef23779917aa711c2c7ea15b6ac7e71572225
Sunil Singh <sunilp896@gmail.com>
2020-02-25 08:10:24 +0530

    Adds more accessor functions to the function's new CFG

--- include/FunctionCFG.h
+++ include/FunctionCFG.h
@@ -1,3 +1,13 @@
+/**
+ * @file FunctionCFG.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-02-22
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 #ifndef _A_FUNCTION_CFG_H_
 #define _A_FUNCTION_CFG_H_
 #include "llvm/IR/Function.h"
@@ -10,6 +20,10 @@
 
 namespace A{
     enum Direction{FORWARD,BACKWARD};
+    /**
+     * @brief 
+     * 
+     */
     class Node{
         unsigned id;
         llvm::BasicBlock* bb;
@@ -23,13 +37,13 @@ namespace A{
             preds = std::vector<llvm::BasicBlock*>(0);
             succs = std::vector<llvm::BasicBlock*>(0);
 
-            for(llvm::succ_iterator itb = succ_begin(bb),ite=succ_end(bb); itb!=ite; ++itb){
-                succs.push_back(*itb);
-            }
-
-            for(llvm::pred_iterator itb = pred_begin(bb),ite=pred_end(bb); itb!=ite; ++itb){
-                preds.push_back(*itb);
-            }
+            if(bb)
+                for(llvm::succ_iterator itb = succ_begin(bb),ite=succ_end(bb); itb!=ite; ++itb)
+                    succs.push_back(*itb);
+                
+            if(bb)
+                for(llvm::pred_iterator itb = pred_begin(bb),ite=pred_end(bb); itb!=ite; ++itb)
+                    preds.push_back(*itb);
         }
         std::string GetName(){return bb->getName().str();}
         llvm::BasicBlock* GetBB(){return bb;}
@@ -38,22 +52,28 @@ namespace A{
         std::vector<llvm::BasicBlock*> GetSuccs(){return succs;}
         int SuccsCount(){return succs.size();}
     };
-    
+    /**
+     * @brief 
+     * 
+     */
     class FunctionCFG{
     public:
         FunctionCFG(llvm::Function*);
-        ~FunctionCFG(){
+        ~FunctionCFG(){}
 
-        }
-
-        std::list<llvm::BasicBlock*> GetHeads();
-        std::list<llvm::BasicBlock*> GetTails();
-        std::list<llvm::BasicBlock*> GetIn(A::Node*,Direction);
-        std::list<llvm::BasicBlock*> GetOut(A::Node*,Direction);
+        Node* GetNode(llvm::BasicBlock* bb){   return BBtoNode[bb];}
+        std::vector<llvm::BasicBlock*> GetHeads();
+        std::vector<llvm::BasicBlock*> GetTails();
+        std::vector<Node*> GetHeadsN();
+        std::vector<Node*> GetTailsN();
+        std::vector<llvm::BasicBlock*> GetIn(A::Node*,Direction);
+        std::vector<llvm::BasicBlock*> GetOut(A::Node*,Direction);
+        std::vector<Node*> GetInN(A::Node*,Direction);
+        std::vector<Node*> GetOutN(A::Node*,Direction);
     private:
-
         unsigned NumBB;
         std::vector<Node*> *Nodes;
+        llvm::DenseMap<llvm::BasicBlock*,Node*> BBtoNode;
         llvm::Function* func;
     };
     
--- src/FunctionCFG.cpp
+++ src/FunctionCFG.cpp
@@ -1,6 +1,11 @@
 #include "FunctionCFG.h"
 #include "llvm/ADT/DepthFirstIterator.h"
 
+/**
+ * @brief Construct a new A:: Function C F G:: Function C F G object
+ * 
+ * @param f 
+ */
 A::FunctionCFG::FunctionCFG(llvm::Function* f){
     this->NumBB = f->size();
     this->func  = f;
@@ -9,46 +14,131 @@ A::FunctionCFG::FunctionCFG(llvm::Function* f){
     for (llvm::df_iterator<llvm::BasicBlock*> I = llvm::df_begin(&func->getEntryBlock()),
                                             IE=llvm::df_end(&func->getEntryBlock()); I != IE; ++I){
         A::Node* node = new A::Node(*I,Nodes->size());
+        BBtoNode[*I] = node;
         Nodes->push_back(node);
     }
 }
 
-std::list<llvm::BasicBlock*> A::FunctionCFG::GetHeads(){
-    std::list<llvm::BasicBlock*> lst;
+/**
+ * @brief 
+ * 
+ * @return std::vector<llvm::BasicBlock*> 
+ */
+std::vector<llvm::BasicBlock*> A::FunctionCFG::GetHeads(){
+    std::vector<llvm::BasicBlock*> lst;
     for(auto node : *Nodes){
         if(!node->PredCount()){
             lst.push_back(node->GetBB());
         }
     }
-    return lst;    
+    return lst;
 }
 
-std::list<llvm::BasicBlock*> A::FunctionCFG::GetTails(){
-    std::list<llvm::BasicBlock*> lst;
+/**
+ * @brief 
+ * 
+ * @return std::vector<llvm::BasicBlock*> 
+ */
+std::vector<llvm::BasicBlock*> A::FunctionCFG::GetTails(){
+    std::vector<llvm::BasicBlock*> lst;
     for(auto node : *Nodes){
         if(!node->SuccsCount()){
             lst.push_back(node->GetBB());
         }
     }
+    return lst;
+}
+        
+/**
+ * @brief 
+ * 
+ * @return std::vector<A::Node*> 
+ */
+std::vector<A::Node*> A::FunctionCFG::GetHeadsN(){
+    std::vector<A::Node*> lst;
+    for(auto node : *Nodes){
+        if(!node->PredCount()){
+            lst.push_back(node);
+        }
+    }
     return lst;    
 }
 
-std::list<llvm::BasicBlock*> A::FunctionCFG::GetIn(A::Node* n,Direction dir){
-    std::list<llvm::BasicBlock*> lst;
+/**
+ * @brief 
+ * 
+ * @return std::vector<A::Node*> 
+ */
+std::vector<A::Node*> A::FunctionCFG::GetTailsN(){
+    std::vector<A::Node*> lst;
+    for(auto node : *Nodes){
+        if(!node->SuccsCount()){
+            lst.push_back(node);
+        }
+    }
+    return lst;    
+}
+
+/**
+ * @brief 
+ * 
+ * @param n 
+ * @param dir 
+ * @return std::vector<llvm::BasicBlock*> 
+ */
+std::vector<llvm::BasicBlock*> A::FunctionCFG::GetIn(A::Node* n,Direction dir){
     if(dir == Direction::FORWARD){
-        std::copy(n->GetPreds().begin(),n->GetPreds().end(),std::back_inserter(lst));
+        return n->GetPreds();
     }else{
-        std::copy(n->GetSuccs().begin(),n->GetSuccs().end(),std::back_inserter(lst));
+        return n->GetSuccs();
+    }
+}
+
+/**
+ * @brief 
+ * 
+ * @param n 
+ * @param dir 
+ * @return std::vector<llvm::BasicBlock*> 
+ */
+std::vector<llvm::BasicBlock*> A::FunctionCFG::GetOut(A::Node* n,Direction dir){
+    if(dir == Direction::FORWARD){
+        return n->GetSuccs();
+    }else{
+        return n->GetPreds();
+    }
+}
+
+/**
+ * @brief 
+ * 
+ * @param n 
+ * @param dir 
+ * @return std::vector<A::Node*> 
+ */
+std::vector<A::Node*> A::FunctionCFG::GetInN(A::Node* n,Direction dir){
+    std::vector<A::Node*> lst;
+    if(dir == Direction::FORWARD){
+        std::transform(n->GetPreds().begin(),n->GetPreds().end(),std::inserter(lst,lst.begin()),[this](llvm::BasicBlock* bb){return GetNode(bb);});
+    }else{
+        std::transform(n->GetSuccs().begin(),n->GetSuccs().end(),std::inserter(lst,lst.begin()),[this](llvm::BasicBlock* bb){return GetNode(bb);});
     }
     return lst;
 }
 
-std::list<llvm::BasicBlock*> A::FunctionCFG::GetOut(A::Node* n,Direction dir){
-    std::list<llvm::BasicBlock*> lst;
+/**
+ * @brief 
+ * 
+ * @param n 
+ * @param dir 
+ * @return std::vector<A::Node*> 
+ */
+std::vector<A::Node*> A::FunctionCFG::GetOutN(A::Node* n,Direction dir){
+    std::vector<A::Node*> lst;
     if(dir == Direction::FORWARD){
-        std::copy(n->GetSuccs().begin(),n->GetSuccs().end(),std::back_inserter(lst));
+        std::transform(n->GetSuccs().begin(),n->GetSuccs().end(),std::inserter(lst,lst.begin()),[this](llvm::BasicBlock* bb){return GetNode(bb);});
     }else{
-        std::copy(n->GetPreds().begin(),n->GetPreds().end(),std::back_inserter(lst));
+        std::transform(n->GetPreds().begin(),n->GetPreds().end(),std::inserter(lst,lst.begin()),[this](llvm::BasicBlock* bb){return GetNode(bb);});
     }
     return lst;    
 }
