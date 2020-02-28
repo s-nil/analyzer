b1746480acd67a7862c2e472951d966f02b350ff
Sunil Singh <sunilp896@gmail.com>
2020-02-28 10:38:49 +0530

    change in type of data member change in the constructor

--- include/NodeData.h
+++ include/NodeData.h
@@ -12,6 +12,7 @@
 #define _A_NODE_DATA_H_
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/IR/BasicBlock.h"
+#include "llvm/Support/raw_ostream.h"
 
 namespace A{
     /**
@@ -24,17 +25,22 @@ namespace A{
     public:
         NodeData(){
             node = nullptr;
+            number = INT_MIN;
+            inFlow = nullptr;
+            outFlow = nullptr;
         }
         NodeData(llvm::BasicBlock* n, NodeData<T>* in){
             node = n;
             if(in) AddIn(in);
             number = INT_MIN;
+            inFlow = nullptr;
+            outFlow = nullptr;
         }
         std::string GetNodeName();
-        T GetInFlow(){   return inFlow;}
-        T GetOutFlow(){  return outFlow;}
-        void SetInFlow(T in){    inFlow = in;}
-        void SetOutFlow(T out){  outFlow = out;}
+        T* GetInFlow(){     return inFlow;}
+        T* GetOutFlow(){    return outFlow;}
+        void SetInFlow(T* in){    inFlow = in;}
+        void SetOutFlow(T* out){  outFlow = out;}
         llvm::SmallVector<NodeData<T>*,0> GetINcomingN(){    return incoming;}
         llvm::SmallVector<NodeData<T>*,0> GetOutgoingN(){    return outgoing;}
         llvm::BasicBlock* GetNode(){    /*if(node)llvm::errs() << ":: "<<node->getName().str()<<'\n';*/ return node;}
@@ -50,9 +56,9 @@ namespace A{
     private:
         llvm::BasicBlock* node;
         /// final in flow after merging all incoming flow data
-        T inFlow;
+        T* inFlow;
         /// out flow after applying transfer function to the inFlow
-        T outFlow;
+        T* outFlow;
         /// data of incoming edges
         llvm::SmallVector<NodeData<T>*,0>  incoming;
         /// data of outgoing edges
